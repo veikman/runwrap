a9b57fb0e165cbf07a783f9bbb091c10b2d26820
Viktor Eikman <viktor.eikman@gmail.com>
2021-05-22 14:12:01 +0200

    Stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 
-pub fn unwrap(raw: &str) -> &str {
-    raw
+pub fn unwrap(raw: &str) -> String {
+    let old: Vec<&str> = raw.split("\n\n").collect();
+    let mut new = String::with_capacity(raw.len());
+    for p in old.iter() {
+        new.push_str(p)
+    }
+    return new;
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,8 +1,24 @@
 use runwrap;
 
-const EMPTY: &str = "";
-
 #[test]
 fn unwrap_empty() {
-    assert_eq!(EMPTY, runwrap::unwrap(EMPTY));
+    const VAL: &str = "";
+    assert_eq!(VAL, runwrap::unwrap(VAL));
+}
+
+#[test]
+fn unwrap_space() {
+    const VAL: &str = " ";
+    assert_eq!(VAL, runwrap::unwrap(VAL));
+}
+
+#[test]
+fn unwrap_1newline() {
+    const VAL: &str = "\n";
+    assert_eq!(VAL, runwrap::unwrap(VAL));
+}
+
+#[test]
+fn unwrap_2newline() {
+    assert_eq!("\n\n", runwrap::unwrap("\n\n"));
 }
