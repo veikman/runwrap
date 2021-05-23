47865a062d3221a5d491bfb75ecaff587fc603c5
Viktor Eikman <viktor.eikman@gmail.com>
2021-05-23 13:35:58 +0200

    Iteration

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
+// lines: Vec<&str> = p.split("\n").collect();
 
 pub fn unwrap(raw: &str) -> String {
     let old: Vec<&str> = raw.split("\n\n").collect();
     let mut new = String::with_capacity(raw.len());
     for p in old.iter() {
-        new.push_str(p)
+        new.push_str(unwrap_paragraph(p));
     }
     return new;
+}
+
+enum ParagraphType {
+    Text,
+}
+
+fn unwrap_paragraph(p: &str) -> &str {
+    match classify(p) {
+        Some(ParagraphType::Text) => p, // TODO: wrap at usize.MAX
+        None => p,
+    }
+}
+
+fn classify(subject: &str) -> Option<ParagraphType> {
+    Some(ParagraphType::Text)
 }
